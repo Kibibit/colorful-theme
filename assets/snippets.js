2ebda3f721bab19df015c6e8c5445d0abb5d5632
Neil Kalman <Neilkalman@gmail.com>
2018-05-13 11:14:56 +0300

    Update snippets.js

--- assets/snippets.js
+++ assets/snippets.js
@@ -23,7 +23,7 @@ function kbCodeSnippet() {
       const codeElement = $(block);
       const lang = (_.find(block.classList, classIsLanguage) || '').replace('language-', '');
       
-      const innerCode = codeElement.find('code').addClass(lang)[0];
+      const innerCode = codeElement.find('pre code').addClass(lang)[0];
   
       // hightlight code (auto detect if needed)
       hljs.highlightBlock(innerCode);
