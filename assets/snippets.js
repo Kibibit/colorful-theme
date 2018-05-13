ae40e6c91180edd95884d2335fba2373a64002d1
Neil Kalman <Neilkalman@gmail.com>
2018-05-13 10:48:16 +0300

    Update snippets.js

--- assets/snippets.js
+++ assets/snippets.js
@@ -1,5 +1,7 @@
 kbCodeSnippet();
 
+colorKibibitTitles();
+
 function colorKibibitTitles() {
     $('h1, h2, h3, h4, h4').each(( index ) => {
       const content = $( this ).html().replace('kibibit', [
