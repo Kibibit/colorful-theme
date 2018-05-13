9d6e9e6e687d97de59c0f74ac04913d045e44ac2
Neil Kalman <Neilkalman@gmail.com>
2018-05-13 11:16:58 +0300

    Update snippets.js

--- assets/snippets.js
+++ assets/snippets.js
@@ -3,19 +3,19 @@ kbCodeSnippet();
 colorKibibitTitles();
 
 function colorKibibitTitles() {
-    $('h1, h2, h3, h4, h4').each(( index ) => {
-      const content = $( this ).html().replace('kibibit', [
-        'k',
-        '<span class="kb-red">i</span>',
-        'b',
-        '<span class="kb-blue">i</span>',
-        'b',
-        '<span class="kb-yellow">i</span>',
-        't'
-      ].join(''));
-      
-      $( this ).html(content);
-    });
+  $('h1, h2, h3, h4, h4').each(( index ) => {
+    const content = $( this ).html().replace('kibibit', [
+      'k',
+      '<span class="kb-red">i</span>',
+      'b',
+      '<span class="kb-blue">i</span>',
+      'b',
+      '<span class="kb-yellow">i</span>',
+      't'
+    ].join(''));
+
+    $( this ).html(content);
+  });
 }
 
 function kbCodeSnippet() {
@@ -24,14 +24,16 @@ function kbCodeSnippet() {
       const lang = (_.find(block.classList, classIsLanguage) || '').replace('language-', '');
       
       const innerCode = codeElement.find('pre code').addClass(lang)[0];
-  
-      // hightlight code (auto detect if needed)
-      hljs.highlightBlock(innerCode);
+        
+        if (innerCode) {
+          // hightlight code (auto detect if needed)
+          hljs.highlightBlock(innerCode);
+        }
     });
   }
 
 // hljs.initHighlightingOnLoad();
 
 function classIsLanguage(className) {
-    return className.startsWith('language-');
+  return className.startsWith('language-');
 }
